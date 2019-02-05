e945132df974589c67534aa3fb55d022212c1063
david-mears <kieukmail@gmail.com>
2019-02-05 15:21:34 +0000

    TDD did it.

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,20 @@
-function fizzBuzz() {
+function FizzBuzz() {
 };
-fizzBuzz.prototype.output = function() {
-  return 1;
+
+FizzBuzz.prototype.output = function() {
+  for (i = 1; i <= 100; i++) {
+    this.calculate(i);
+  }
+};
+
+FizzBuzz.prototype.calculate = function(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log('fizzbuzz')
+  } else if (num % 5 === 0) {
+    console.log('buzz')
+  } else if (num % 3 ===0) {
+    console.log('fizz')
+  } else {
+    console.log(num)
+  }
 };
